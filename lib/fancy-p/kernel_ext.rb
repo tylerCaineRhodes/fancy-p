13eff51bb9e37202e7f7a5198c9a47cce7918aaa
Tyler Rhodes <rhodetyl000@gmail.com>
2024-07-15 15:38:26 -0500

    Enable usage of n-length strings for `fp` method

--- lib/fancy-p/kernel_ext.rb
+++ lib/fancy-p/kernel_ext.rb
@@ -4,29 +4,30 @@ module Kernel
   def method_missing(method_name, *args, &)
     super unless method_name.to_s =~ /^p.$/
 
-    char = method_name.to_s[1]
     self.class.define_method(method_name) do |*forwarded_args|
-      delimiter = char * 100
-      p delimiter
-      p(*forwarded_args)
-      p delimiter
+      char = method_name.to_s[1]
+      print_surrounding(char, *forwarded_args)
     end
     send(method_name, *args)
   end
 
-  def fp(str, *args, length: 100)
-    delimiter = str[0] * length
+  def print_surrounding(str, *args, length: 100)
+    delimiter = str * length
     p delimiter
     p(*args)
     p delimiter
   end
 
-  def fp_factory(char, length: 100)
+  def fp(str, *args, length: 100)
+    delimiter = str * length
+    p delimiter
+    p(*args)
+    p delimiter
+  end
+
+  def fp_factory(str, length: 100)
     lambda { |*args|
-      delimiter = char * length
-      p delimiter
-      p(*args)
-      p delimiter
+      fp(str, *args, length:)
     }
   end
 end
