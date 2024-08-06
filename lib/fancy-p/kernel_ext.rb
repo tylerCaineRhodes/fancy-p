fad6c7aca85165aff63a7fadc1e33c5891ad1c94
Tyler Rhodes <rhodetyl000@gmail.com>
2024-08-05 22:34:29 -0700

    Ignore delimiter quotes

--- lib/fancy-p/kernel_ext.rb
+++ lib/fancy-p/kernel_ext.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 module Kernel
-  def method_missing(method_name, *args, &)
+  def method_missing(method_name, *args, &block)
     super unless method_name.to_s =~ /^p.$/
 
-    self.class.define_method(method_name) do |*forwarded_args|
+    define_singleton_method(method_name) do |*forwarded_args|
       char = method_name.to_s[1]
       fp(char, *forwarded_args)
     end
@@ -13,9 +13,9 @@ module Kernel
 
   def fp(str, *args, length: 100)
     delimiter = str * length
-    p delimiter
+    puts delimiter
     p(*args)
-    p delimiter
+    puts delimiter
   end
 
   def fp_factory(str, length: 100)
@@ -25,7 +25,7 @@ module Kernel
   end
 
   def fp_define(str, length: 100, method_name: str.strip)
-    self.class.define_method("fp_#{method_name.strip}") do |*args|
+    define_singleton_method("fp_#{method_name.strip}") do |*args|
       fp(str, *args, length:)
     end
   end
